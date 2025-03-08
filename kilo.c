866305e7689a55f1b8b9cc459c4288775816d39d
deanmartodikromo <dev.martodikromo99@gmail.com>
2025-03-08 18:37:06 -0300

    Chapter 3 - Clear the screen on exit

--- kilo.c
+++ kilo.c
@@ -5,9 +5,13 @@
 #include <termios.h>
 #include <unistd.h>
 
+#define CTRL_KEY(k) ((k) & 0x1f)
+
 struct termios orig_termios;
 
 void die(const char *s) {
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\xb[H", 3);
     perror(s);
     exit(1);
 }
@@ -31,17 +35,38 @@ void enableRawMode() {
     if (tcsetattr(STDIN_FILENO, TCIFLUSH, &raw) == -1) die("tcsetattr");
 }
 
+char editorReadKey() {
+    int nread;
+    char c;
+    while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+        if (nread == -1 && errno != EAGAIN) die("read");
+    }
+    return c;
+}
+
+void editorRefreshScreen() {
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\xb[H", 3);
+}
+
+void editorProcessKeyPress() {
+    char c = editorReadKey();
+    switch (c) {
+        case CTRL_KEY('q'):
+            write(STDOUT_FILENO, "\x1b[2J", 4);
+            write(STDOUT_FILENO, "\x1b[H", 3);
+            exit(0);
+            break;
+        default:
+            break;
+    }
+}
+
 int main() {
     enableRawMode();
     while (1) {
-        char c = '\0';
-        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
-        if (iscntrl(c)) {
-            printf("%d\r\n", c);
-        } else {
-            printf("%d ('%c')\r\n", c, c);
-        }
-        if (c == 'q') break;
+        editorRefreshScreen();
+        editorProcessKeyPress();
     }
     return 0;
 }
